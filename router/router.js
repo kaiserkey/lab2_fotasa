abc908c3f79944c785de4003e1283c3d470e238a
DanielSan <kaiserkey2@gmail.com>
2022-11-29 21:59:59 -0300

    se agrego un footer y navbar

--- router/router.js
+++ router/router.js
@@ -1,9 +1,11 @@
 'use strict'
+
 const express = require('express'),
         router = express.Router(),
         { err404 } = require('../helppers/helppers')
 
 router.get( '/', (req,res)=>{ res.render('index') } )
+        
         .use( err404 )
 
 
